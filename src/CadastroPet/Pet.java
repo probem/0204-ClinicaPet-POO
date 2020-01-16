e24c2ed8aa1eb1955d7d5261282b3692be17cd26
Gabriel Roger <gabriel.nroger@gmail.com>
2020-01-16 17:07:36 -0300

    adicionar encapsulamento pet

--- src/CadastroPet/Pet.java
+++ src/CadastroPet/Pet.java
@@ -11,6 +11,62 @@ public class Pet {
     private Responsavel dono;
     private String tipoCuidado;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Responsavel getDono() {
+        return dono;
+    }
+
+    public void setDono(Responsavel dono) {
+        this.dono = dono;
+    }
+
+    public String getTipoCuidado() {
+        return tipoCuidado;
+    }
+
+    public void setTipoCuidado(String tipoCuidado) {
+        this.tipoCuidado = tipoCuidado;
+    }
+
     public Pet(String nome, String especie, String raca, int idade, String cor, Responsavel dono, String tipoCuidado) {
         this.nome = nome;
         this.especie = especie;
